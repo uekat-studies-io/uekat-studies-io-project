bec3c9438e01bef190d931fdaf7f080224d54ec0
oreganoli <3611916+oreganoli@users.noreply.github.com>
2023-06-03 19:56:06 +0200

    get seed as CLI param

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var map = Map.Generate(404);
+if (args.Count() < 1)
+{
+    Console.WriteLine("Error: no seed argument was provided.");
+    return;
+}
+var seedOk = int.TryParse(args[0], out var seed);
+if (!seedOk)
+{
+    Console.WriteLine("Error: invalid seed provided. The random seed must be a valid integer.");
+    return;
+}
+var map = Map.Generate(seed);
 var solution = Solver.Solve(map);
 Console.WriteLine($"TOTAL SOLUTION: {solution} kilometers");
